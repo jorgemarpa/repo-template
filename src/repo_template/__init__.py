d1af11694c129f702dc6497c10f2c3be6dde07b1
Jorge Martinez-Palomera <jorgemarpa@ug.uchile.cl>
2025-03-27 18:23:58 -0400

    v0.1.2

--- src/repo_template/__init__.py
+++ src/repo_template/__init__.py
@@ -4,7 +4,7 @@ from __future__ import absolute_import
 
 import os
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 PACKAGEDIR = os.path.abspath(os.path.dirname(__file__))
 
 from .myclass import MyClass  # noqa
